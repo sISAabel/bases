dfac99e456687cdf2593989523dbf671d7135cbe
Moisés Lodeiro <moises.lodeiro@gmail.com>
2023-01-05 00:06:48 +0000

    Añadido hello world

--- ejercicios/helloWorld/hello.js
+++ ejercicios/helloWorld/hello.js
@@ -1,7 +1,13 @@
 
+/**
+ * Dado un nombre, devuelve un saludo de buenos días
+ *
+ * La función obtiene un nombre como parámetro y devuelve un saludo
+ * @param name - una string que representa el nombre
+ * @returns Buenos días, xyz!
+ */
 function helloWorld(name) {
   // Inicio código
-  return `¡Buenos días ${name}!`;
   // Fin código
 }
 
