bfdde56f1dc6b71f8bd5fd70635384938c439613
Moisés Lodeiro <moises.lodeiro@gmail.com>
2023-01-05 01:15:21 +0000

    Añadido helloBye

---
+++ ejercicios/helloBye/helloBye.js
@@ -0,0 +1,14 @@
+
+/**
+ * Toma una cadena nombre y un número num (0 ó 1) y devuelva "Hello" + nombre si num es 1,
+ * en caso contrario devuelva "Bye" + nombre.
+ * @param name - a string
+ * @param num - "Hello" or "Bye", depending on the name
+ */
+
+function helloBye(name, text) {
+  // Inicio código
+  // Fin código
+}
+
+module.exports = helloBye;
---
+++ ejercicios/helloBye/helloBye.test.js
@@ -0,0 +1,10 @@
+const helloBye = require("./helloBye");
+
+test("helloBye 1", () => {
+  expect(helloBye('Moisés', 1)).toBe('Hello Moisés');
+});
+
+test("helloBye 2", () => {
+  expect(helloBye('Manz', 0)).toBe('Bye Manz');
+});
+
