3378089f8bfdeb6941c203f30685a1845a6b3ea4
Moisés Lodeiro <moises.lodeiro@gmail.com>
2023-01-05 01:38:51 +0000

    Añadido damage pokemon

---
+++ ejercicios/damage/damage.js
@@ -0,0 +1,15 @@
+/**
+ * Dado un tipo de pokémon, el tipo del pokémon al que ataca, la estadística de
+ * ataque del pokémon atacante y la estadística de defensa del pokémon defensor,
+ * calcula el daño que hace el ataque.
+ * @param yourType - String, the type of your character
+ * @param opponentType - the type of the opponent's Pokémon.
+ * @param attack - the attack of the attacking Pokemon
+ * @param defense - the defense of the opponent
+ */
+function calculateDamage(yourType, opponentType, attack, defense){
+  // inicio código
+  // fin código
+}
+
+module.exports = calculateDamage;
---
+++ ejercicios/damage/damage.test.js
@@ -0,0 +1,29 @@
+const calculateDamage = require("./damage");
+
+test("Fuego contra agua", () => {
+  expect(calculateDamage("fire", "water", 100, 100)).toBe(25);
+});
+
+test("Hierba contra agua", () => {
+  expect(calculateDamage("grass", "water", 100, 100)).toBe(100);
+});
+
+test("Eléctrico contra fuego", () => {
+  expect(calculateDamage("electric", "fire", 100, 100)).toBe(50);
+});
+
+test("Hierba contra eléctrico", () => {
+  expect(calculateDamage("grass", "electric", 57, 19)).toBe(150);
+});
+
+test("Hierba contra agua", () => {
+  expect(calculateDamage("grass", "water", 40, 40)).toBe(100);
+});
+
+test("Hierba contra fuego", () => {
+  expect(calculateDamage("grass", "fire", 35, 5)).toBe(175);
+});
+
+test("Fuego contra eléctrico", () => {
+  expect(calculateDamage("fire", "electric", 10, 2)).toBe(250);
+});
