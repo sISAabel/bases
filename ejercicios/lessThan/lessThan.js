0e503b2a38c0e69b175497990c8dd82cf1c120fa
Moisés Lodeiro <moises.lodeiro@gmail.com>
2023-01-05 01:01:45 +0000

    Añadido el less than or equal zero

---
+++ ejercicios/lessThan/lessThan.js
@@ -0,0 +1,13 @@
+/**
+ * Dado un número, devuelve true si es menor o igual que cero,
+ * en caso contrario devuelve false.
+ * @param num - a number
+ * @returns true
+ */
+
+function lessThanOrEqualZero(num){
+  // inicio código
+  // fin código
+}
+
+module.exports = lessThanOrEqualZero;
---
+++ ejercicios/lessThan/lessThan.test.js
@@ -0,0 +1,14 @@
+const lessThanOrEqualZero = require("./lessThan");
+
+
+test("lessThanOrEqualZero 1", () => {
+  expect(lessThanOrEqualZero(5)).toBe(false);
+});
+
+test("lessThanOrEqualZero 2", () => {
+  expect(lessThanOrEqualZero(0)).toBe(true);
+});
+
+test("lessThanOrEqualZero 3", () => {
+  expect(lessThanOrEqualZero(-5)).toBe(true);
+});
